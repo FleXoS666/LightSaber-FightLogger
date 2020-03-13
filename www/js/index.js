b40c3a4b79b5467b8a88e0a01ba33bea5e01beb5
FleXoS666 <yourivanth@hotmail.fr>
2020-03-13 17:02:55 +0100

    c'est mieux mais c'est pas encore ça

--- www/js/index.js
+++ www/js/index.js
@@ -6,6 +6,12 @@ document.addEventListener('deviceready', function(event){
     initTable();
     DisplayScore();
     displayTeamList();
+    var idA=null;
+    var idB=null;
+    var scoreA=0;
+    var scoreB=0;
+    var texteA = null;
+    var texteB = null;
     console.log('device is ready');
 
 
@@ -22,42 +28,45 @@ document.addEventListener('deviceready', function(event){
 //  Selection du combattant 1 et affichage de son nom
 var liste1 = document.getElementById("selectTeam1");
 liste1.addEventListener("change", function(){
-    var scoreA=0;
+
     var nomCombattant1 = document.querySelector(".teamAName");
-    var idTeamA = liste1.options[liste1.selectedIndex].value;
-    var texteA = liste1.options[liste1.selectedIndex].text;
+    var idTeamA = idA;
+    var scoreA=0;
+    var scoreB=0;
+    
     
     console.log(idTeamA);
     nomCombattant1.innerHTML = texteA;
-
-    var btn1A= document.querySelector(".add1A");
-    var btn2A= document.querySelector(".add2A");
-    var scoreADisplay= document.querySelector(".scoreA");
     scoreADisplay.innerHTML = scoreA;
-    btn1A.addEventListener("click", function(){
-        console.log('j ai cliqué sur 1');
-        scoreA++;
-        scoreADisplay.innerHTML= scoreA;
 
-        addPoints(scoreA,idTeamA,texteA);
-    });
-
-    btn2A.addEventListener("click", function(){
-        console.log('j ai cliqué sur 2');
-        scoreA = scoreA+2
-        scoreADisplay.innerHTML= scoreA;
-        
-        addPoints(scoreA,idTeamA,texteA);
-    });
+});
+var btn1A= document.querySelector(".add1A");
+var btn2A= document.querySelector(".add2A");
+var scoreADisplay= document.querySelector(".scoreA");
+btn1A.addEventListener("click", function(){
+    console.log('j ai cliqué sur 1');
+    scoreA++;
+    scoreADisplay.innerHTML= scoreA;
+    texteA = liste1.options[liste1.selectedIndex].text;
+    idTeamA = liste1.options[liste1.selectedIndex].value;
+    addPoints(scoreA,idTeamA,texteA);
 });
 
+btn2A.addEventListener("click", function(){
+    console.log('j ai cliqué sur 2');
+    scoreA = scoreA+2
+    scoreADisplay.innerHTML= scoreA;
+    texteA = liste1.options[liste1.selectedIndex].text;
+    idTeamA = liste1.options[liste1.selectedIndex].value;
+    addPoints(scoreA,idTeamA,texteA);
+});
 //  Selection du combattant 2 et affichage de son nom
 var liste2 = document.getElementById("selectTeam2");
 liste2.addEventListener("change", function(){
     var scoreB=0;
     var nomCombattant2 = document.querySelector(".teamBName");
-    var idTeamB = liste2.options[liste2.selectedIndex].value;
-    var texteB = liste2.options[liste2.selectedIndex].text;
+    var idTeamB = idB;
+    
 
     console.log(idTeamB);
     nomCombattant2.innerHTML = texteB;
@@ -70,7 +79,8 @@ liste2.addEventListener("change", function(){
         console.log('j ai cliqué sur 1');
         scoreB++;
         scoreBDisplay.innerHTML= scoreB;
-        
+        idTeamB = liste2.options[liste2.selectedIndex].value;
+        texteB = liste2.options[liste2.selectedIndex].text;
         addPoints(scoreB,idTeamB,texteB);
     });
 
@@ -78,34 +88,10 @@ liste2.addEventListener("change", function(){
         console.log('j ai cliqué sur 2');
         scoreB = scoreB+2
         scoreBDisplay.innerHTML= scoreB;
-
+        idTeamB = liste2.options[liste2.selectedIndex].value;
+        texteB = liste2.options[liste2.selectedIndex].text;
         addPoints(scoreB,idTeamB,texteB);
     });
 });
-
-
-// var btn1B= document.querySelector(".add1B");
-// var btn2B= document.querySelector(".add2B");
-// var scoreBDisplay= document.querySelector(".scoreB");
-// var scoreB=0;
-
-
-
-// btn1B.addEventListener("click", function(){
-//     console.log('j ai cliqué sur 1');
-//     scoreB++;
-//     scoreBDisplay.innerHTML= scoreB;
-//     idTeam=2;
-//     addPoints(scoreB,idTeam);
-// });
-
-// btn2B.addEventListener("click", function(){
-//     console.log('j ai cliqué sur 2');
-//     scoreB = scoreB+2
-//     scoreBDisplay.innerHTML= scoreB;
-//     idTeam=2;
-//     addPoints(scoreB,idTeam);
-// });
-
 
 });
